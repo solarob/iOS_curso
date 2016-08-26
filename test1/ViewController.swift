13bd6a599b7d54694bb5619305f30e8a11ac8a50
benjamin solar <benjamin@colorpicker.cl>
2016-08-25 23:57:25 -0300

    proyecto 1 curso iOS

--- test1/ViewController.swift
+++ test1/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var messageLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeMessage(sender: AnyObject) {
+        messageLabel.text = "Hola \(nameTextField.text!)"
+        view.endEditing(true)
+    }
 
 }
 
